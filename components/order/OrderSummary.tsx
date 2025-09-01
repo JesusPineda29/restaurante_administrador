1f5bfc6c019488b28d10fe301270ac1f2cd3f8a2
JesusPineda29 <jesuspineda1429@gmail.com>
2025-09-01 14:43:10 -0600

    agregando función createOrder

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,32 +3,52 @@ import { useStore } from "@/src/store"
 import ProductDetails from "./ProductDetails"
 import { useMemo } from "react"
 import { formatCurrency } from "@/src/utils"
+import { createOrder } from "@/actions/create-order-action"
 
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order)
 
-  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0) , [order])
+  const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+  const handleCreateorder = () => {
+    console.log('desde el handle')
+
+    createOrder()
+  }
 
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5">
-        <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+      <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
-        {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio</p> : (
-          <div className="mt-5">
-            {order.map(item => (
-              <ProductDetails 
-                key={item.id}
-                item={item}
-              />
-            ))}
+      {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio</p> : (
+        <div className="mt-5">
+          {order.map(item => (
+            <ProductDetails
+              key={item.id}
+              item={item}
+            />
+          ))}
 
-            <p className="text-2xl mt-20 text-center">
-              Total a pagar: {''}
-              <span className="font-bold">{formatCurrency(total)}</span>
-            </p>
-          </div>
-        )}
+          <p className="text-2xl mt-20 text-center">
+            Total a pagar: {''}
+            <span className="font-bold">{formatCurrency(total)}</span>
+          </p>
+
+          <form
+            className="w-full mt-10 space-y-5"
+            action={handleCreateorder}
+          >
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value='Confirmar Pedido'
+            />
+
+          </form>
+
+        </div>
+      )}
     </aside>
   )
 }
